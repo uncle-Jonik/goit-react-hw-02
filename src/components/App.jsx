738795653dac974ee664d76f8182424ed8868e95
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-01-19 21:45:05 +0200

    + feedback info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,18 @@ import css from './App.module.css';
 import { useState, useEffect } from 'react';
 import { Description } from './Description/Description';
 
+const config = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
 const localStorage = () => {
   const savedObject = window.localStorage.getItem('settings');
   if (savedObject !== null) {
     return JSON.parse(savedObject);
   }
-  return {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  return config;
 };
 
 export const App = () => {
@@ -24,7 +26,9 @@ export const App = () => {
   const handleClick = value => {
     setCount({ ...count, [value]: count[value] + 1 });
   };
-
+  console.log(config);
+  console.log(count);
+  console.log();
   return (
     <div className={css.wrapper}>
       <Description />
@@ -34,9 +38,15 @@ export const App = () => {
         <button onClick={() => handleClick('bad')}>bad</button>
       </div>
       <div>
-        <p>good: {count.good}</p>
-        <p>neutral: {count.neutral}</p>
-        <p>bad: {count.bad}</p>
+        {config === count ? (
+          <p>No feedback yet</p>
+        ) : (
+          <>
+            <p>good: {count.good}</p>
+            <p>neutral: {count.neutral}</p>
+            <p>bad: {count.bad}</p>
+          </>
+        )}
       </div>
     </div>
   );
